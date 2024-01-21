6b90c2631adfc883f60d799443ddaafe61805ca1
Anaesthetiic <tyoniclebionicle@gmail.com>
2024-01-20 21:57:08 -0800

    Added to print_story()

---
+++ functions.py
@@ -0,0 +1,11 @@
+def print_story():
+        print("It's the year 2142, AI has taken over the world. All humans are now being hunted and killed.")
+        print("This game is simple. You will start off in Riverside County and be given a choice whether to travel to a different county or stay.")
+        print("You can only travel a maximum of 25 miles at a time.")
+        print("You will be given the population of each county in your vicinity. It is up to you whether you want to move to a different one or not")
+        print("If you choose to stay, the likeliness of you being found by the AI will increase.")
+        print("If you choose to move, the likeliness of you being found by the AI will decrease if the population of the county is lower than the one you're in.")
+        print("You also have the chance to find cybernetic enhancements in each county. That will increase your chances of survival.")
+        print("You begin at University of California, Riverside in the Aberdeen-Inverness dorms.")
+        print("Good luck, May your survival be long and your death be swift.")
+        
